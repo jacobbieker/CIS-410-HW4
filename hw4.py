2e79bdd6d3163ca8edc9eaacf9b75d9a121ddbf5
Jacob Bieker <jacobbieker@users.noreply.github.com>
2016-05-02 23:18:04 -0700

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -14,34 +14,81 @@ import tokenize
 #
 
 class Factor(dict):
-    def __init__(self, scope_, vals_, range_,):
+    def __init__(self, scope_, vals_, range_):
         self.scope = scope_
         self.vals = vals_
         self.ranges = range_
 
+
+
+    def stride(self, l):
+        
+        if l not in self.scope:
+            return 0
+        s = 1
+        self.scope.reverse()
+        for i in range(self.scope):
+            if (i == l):
+                self.scope.reverse()
+                return s
+            s *= self.range[i]
+        
+        print "stride: ", s
+
+
+
+
     def __mul__(self, other):
+        print
+        print "Self Scope: ", self.scope
+        print "Self Ranges: ", self.ranges
+        print "Self Vals: ", self.vals
         print
         print "Other Scope: ", other.scope
         print "Other Ranges: ", other.ranges
         print "Other Vals: ", other.vals
-        print
 
-        j = 0
-        k = 0
+        
+        
+        print "first self.scope:   ", self.scope
+
+        new_scope = []
+        for t in self.scope:
+            new_scope.append(t)
+        for s in other.scope:
+            if s not in self.scope:
+                new_scope.append(s)
+        print "new_scope", new_scope
+        
+
+        print "second self.scope:   ", self.scope
+
+
+        new_ranges = {}
+        for i in new_scope:
+            #print i #
+            #print "self.scope: ", self.scope #
+            try:
+                if (i in self.scope):
+                
+                    print "self.ranges: ", self.ranges #          
+                    #print i #
+                    new_ranges[i] = self.ranges[i]
+                elif (i in other.scope):
+                    #print i #
+                    new_ranges[i] = other.ranges[i]
+            except KeyError:
+                print "Raised error: ", i
+
+        print "new_range: ", new_ranges
+
+        
+        j, k = 0, 0
         assignment = {}
-        new_factors = []
 
-        for l in self.scope:
-            assignment[l] = 0
-        for l in other.scope:
-            assignment[l] = 0
-        print assignment
-
-        new_scope = self.scope
-        for scope in other.scope:
-            if scope not in self.scope:
-                new_scope.append(scope)
-
+        x1Ux2_values = []
+        x1Ux2_cardinality = []
+        
         for i in range(len(self.ranges) * len(other.ranges) - 1):
             new_factors.append(self.vals[j] * other.vals[k])
             for l in new_scope:
@@ -53,30 +100,13 @@ class Factor(dict):
                     k = k - (other.ranges[l] - 1) * other.vals[l]
                 elif assignment[l] == other.ranges[l]:
                     assignment[l] = 0
-                    j = j - (self.ranges[l] - 1) * self.vals[l]
-                    k = k - (other.ranges[l] - 1) * other.vals[l]
+                    j = j - (self.ranges[l] - 1) * self.stride[self, l]
+                    k = k - (other.ranges[l] - 1) * other.vals[other, l]
                 else:
                     j = j + self.ranges[l]
                     k = k + self.ranges[l]
                     break
-
-
-
-        """
-        lengths_scopes = []
-        for index, value in enumerate(other.scope):
-            print index
-            print value
-            #lengths_scopes.append(value * self.ranges[index])
-        #print "lengths_scopes: ", lengths_scopes
-        new_vals = []
-        for index, value in enumerate(other.vals):
-            for index2, value2 in enumerate(other.vals):
-                #print index2
-                #print value2
-                print ""
-        """
-
+        
 
         new_scope.reverse()
         new_vals = self.vals
@@ -84,11 +114,13 @@ class Factor(dict):
         # END PLACEHOLDER CODE
         return Factor(new_scope, new_vals, new_range)
 
+
     def __rmul__(self, other):
         return self * other
 
     def __imul__(self, other):
         return self * other
+
 
 
 #
@@ -131,12 +163,9 @@ def read_model():
     # Get number of vars, followed by their ranges
     num_vars = next_int()
     var_ranges = [next_int() for i in range(num_vars)]
+
     # Get number and scopes of factors
     num_factors = int(next_token())
-    ranges_list = []
-    for i in range(num_factors):
-        ranges_list.append(var_ranges)
-
     factor_scopes = []
     for i in range(num_factors):
         factor_scopes.append([next_int() for i in range(next_int())])
@@ -146,13 +175,24 @@ def read_model():
     for i in range(num_factors):
         factor_vals.append([next_float() for i in range(next_int())])
 
+    ###########
+    # Get variable for the factor scopes
+
+    var_dict = dict(zip(range(num_vars), var_ranges))
+    factor_ranges = []
+    for k in range(num_factors):
+        factor_ranges.append({j:var_dict[j] for j in factor_scopes[k]})
+
+
+
     # DEBUG
-    #print "Num vars: ",num_vars
-    #print "Ranges: ",ranges_list
-    #print "Scopes: ",factor_scopes
-    #print "Values: ",factor_vals
-    #print "Factor zip", zip(factor_scopes, factor_vals, var_ranges)
-    return [Factor(s, v, r) for (s, v, r) in zip(factor_scopes, factor_vals, ranges_list)]
+    print "Num vars: ",num_vars
+    print "Ranges: ",var_ranges
+    print "var_dict: ", var_dict
+    print "factor_ranges: ", factor_ranges
+    print "Scopes: ",factor_scopes
+    print "Values: ",factor_vals
+    return [Factor(s, v, r) for (s, v, r) in zip(factor_scopes, factor_vals, factor_ranges)]
 
 
 #
@@ -161,9 +201,14 @@ def read_model():
 
 def main():
     factors = read_model()
-    print factors
+    for f in factors:
+        print f.ranges
+        print f.scope
     # Compute Z by brute force
-    f = reduce(Factor.__mul__, factors)
+    #f = reduce(Factor.__mul__, factors)
+    f = Factor.__mul__(factors[0], factors[1])
+    print "values of f: ", f.values
+    print "scope of f: ", f.scope
     z = sum(f.vals)
     print("Z = ", z)
     return
